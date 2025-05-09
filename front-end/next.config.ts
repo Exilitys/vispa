606f343913f7828c3ca5d8839555f97d6430ce11
Carlo <jonathancarlo20@gmail.com>
2025-05-09 23:54:08 +0700

    idk

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -3,11 +3,11 @@ import { redirect } from "next/dist/server/api-utils";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  supabase: {
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    key: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    redirect: false,
-  },
+  // supabase: {
+  //   url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  //   key: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  //   redirect: false,
+  // },
   images: {
     domains: ["1drv.ms"],
   },
